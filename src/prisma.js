ec3761a9837ceb64c0732b37dc1932a9e8ced21a
Thiago Lima <thitheguy@gmail.com>
2020-05-07 01:05:21 -0300

    Adding Mutations with Prisma

--- src/prisma.js
+++ src/prisma.js
@@ -1,4 +1,6 @@
-import { Prisma } from 'prisma-binding'
+import {
+    Prisma
+} from 'prisma-binding'
 
 const prisma = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
@@ -11,4 +13,37 @@ prisma.query.users(null, '{ id name email posts { id title }}').then((data) => {
 
 prisma.query.comments(null, '{ id text author { id name } post { id title body }}').then((data) => {
     console.log(JSON.stringify(data, undefined, 2))
+})
+
+prisma.mutation.createPost({
+    data: {
+        title: "GraphQL 101",
+        body: "",
+        published: false,
+        author: {
+            connect: {
+                id: "ck9w7fp6300470987qaocktuy"
+            }
+        }
+    }
+}, '{ id title body published }').then((data) => {
+    console.log(data)
+    return prisma.query.users(null, '{ id name posts { id title } }')
+}).then((data) => {
+    console.log(JSON.stringify(data, undefined, 2))
+})
+
+
+prisma.mutation.updatePost({
+    where: {
+        id: "ck9w7hvl5005u0987qcy9cq40"
+    },
+    data: {
+        body: "This is how to get started with Graphql...",
+        published: true
+    }
+}, '{ id }').then(() => {
+    return prisma.query.posts(null, '{ id title body published }')
+}).then((data) => {
+    console.log(data)
 })
