1253446efb3561769ad8540015b86f0bbfdce06a
Thiago Lima <thitheguy@gmail.com>
2020-05-07 00:09:42 -0300

    Integrating PstgreSQL to Node + Prisma

--- src/prisma.js
+++ src/prisma.js
@@ -4,3 +4,7 @@ const prisma = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
     endpoint: 'http://localhost:4466'
 })
+
+prisma.query.users(null, '{ id name email }').then((data) => {
+    console.log(data)
+})
