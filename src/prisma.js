2b85225174e81fa1431a28a9da1c4adfd83a7a06
Thiago Lima <thitheguy@gmail.com>
2020-05-07 00:27:32 -0300

    Adding prisma.queries

--- src/prisma.js
+++ src/prisma.js
@@ -5,6 +5,10 @@ const prisma = new Prisma({
     endpoint: 'http://localhost:4466'
 })
 
-prisma.query.users(null, '{ id name email }').then((data) => {
+prisma.query.users(null, '{ id name email posts { id title }}').then((data) => {
+    console.log(JSON.stringify(data, undefined, 2))
+})
+
+prisma.query.comments(null, '{ id text author { id name } post { id title body }}').then((data) => {
     console.log(data)
 })
