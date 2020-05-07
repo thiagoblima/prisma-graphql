5cb05b8fb1209f75991d1c038ee896b66a5bd59e
Thiago Lima <thitheguy@gmail.com>
2020-05-07 14:15:10 -0300

    Adding some queries

--- src/prisma.js
+++ src/prisma.js
@@ -15,39 +15,6 @@ prisma.query.comments(null, '{ id text author { id name } post { id title body }
     console.log(JSON.stringify(data, undefined, 2))
 })
 
-prisma.mutation.createPost({
-    data: {
-        title: "GraphQL 101",
-        body: "",
-        published: false,
-        author: {
-            connect: {
-                id: "ck9w7fp6300470987qaocktuy"
-            }
-        }
-    }
-}, '{ id title body published }').then((data) => {
-    console.log(data)
-    return prisma.query.users(null, '{ id name posts { id title } }')
-}).then((data) => {
-    console.log(JSON.stringify(data, undefined, 2))
-})
-
-
-prisma.mutation.updatePost({
-    where: {
-        id: "ck9w7hvl5005u0987qcy9cq40"
-    },
-    data: {
-        body: "This is how to get started with Graphql...",
-        published: true
-    }
-}, '{ id }').then(() => {
-    return prisma.query.posts(null, '{ id title body published }')
-}).then((data) => {
-    console.log(data)
-})
-
 // prisma.query prisma.mutation prisma.subscription prisma.exists
 
 const createPostForUser = async (authorId, data) => {
