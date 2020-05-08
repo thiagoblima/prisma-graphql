1e5e38999e0676e54a721ae4559ca63cce737df8
Thiago Lima <thitheguy@gmail.com>
2020-05-08 02:08:47 -0300

    Adding PostgreSQL / Prisma connection to the project

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Subscription from './resolvers/Subscription'
 import User from './resolvers/User'
 import Post from './resolvers/Post'
 import Comment from './resolvers/Comment'
-import './prisma'
+import prisma from './prisma'
 
 const pubsub = new PubSub()
 
@@ -24,7 +24,8 @@ const server = new GraphQLServer({
     },
     context: {
         db,
-        pubsub
+        pubsub,
+        prisma
     }
 })
 
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,15 +1,17 @@
 const Query = {
-    users(parent, args, { db }, info) {
-        if (!args.query) {
+    users(parent, args, { prisma }, info) {
+        return prisma.query.users(null, info)
+        /* if (!args.query) {
             return db.users
         }
 
         return db.users.filter((user) => {
             return user.name.toLowerCase().includes(args.query.toLowerCase())
-        })
+        }) */
     },
-    posts(parent, args, { db }, info) {
-        if (!args.query) {
+    posts(parent, args, { prisma }, info) {
+        return prisma.query.posts(null, info)
+       /*  if (!args.query) {
             return db.posts
         }
 
@@ -17,7 +19,7 @@ const Query = {
             const isTitleMatch = post.title.toLowerCase().includes(args.query.toLowerCase())
             const isBodyMatch = post.body.toLowerCase().includes(args.query.toLowerCase())
             return isTitleMatch || isBodyMatch
-        })
+        }) */
     },
     comments(parent, args, { db }, info) {
         return db.comments
