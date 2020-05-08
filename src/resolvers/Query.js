17dda09962d5ce982dc713ec3082114adf6f5a2d
Thiago Lima <thitheguy@gmail.com>
2020-05-08 02:33:17 -0300

    Adding query arguments for the database`

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,25 +1,39 @@
 const Query = {
     users(parent, args, { prisma }, info) {
-        return prisma.query.users(null, info)
-        /* if (!args.query) {
-            return db.users
+        const opArgs = {}
+        
+        if (args.query) {
+           opArgs.where = {
+              OR:[{
+                  name_contains: args.query
+              },
+              {
+                  email_contains: args.query
+              }]
+           }
         }
 
-        return db.users.filter((user) => {
-            return user.name.toLowerCase().includes(args.query.toLowerCase())
-        }) */
+        return prisma.query.users(opArgs, info)
+     
     },
     posts(parent, args, { prisma }, info) {
-        return prisma.query.posts(null, info)
-       /*  if (!args.query) {
-            return db.posts
-        }
+        const opArgs = {}
 
-        return db.posts.filter((post) => {
-            const isTitleMatch = post.title.toLowerCase().includes(args.query.toLowerCase())
-            const isBodyMatch = post.body.toLowerCase().includes(args.query.toLowerCase())
-            return isTitleMatch || isBodyMatch
-        }) */
+        if (args.query) {
+            opArgs.where = {
+                OR: [
+                    {
+                        title_contains: args.query
+                    },
+                    {
+                        body_contains: args.query
+                    }
+                ]
+            }
+        }
+       
+        return prisma.query.posts(opArgs, info)
+       
     },
     comments(parent, args, { db }, info) {
         return db.comments
