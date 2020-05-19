cb61914c47aa9d69c29ba7aeba736ce11824c7ab
Thiago Lima <thitheguy@gmail.com>
2020-05-19 01:37:53 -0300

    Adding Secret Token

--- src/prisma.js
+++ src/prisma.js
@@ -4,7 +4,8 @@ import {
 
 const prisma = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
-    endpoint: 'http://localhost:4466'
+    endpoint: 'http://localhost:4466',
+    secret: '61133F2EABE79CA4475F9CAB14124'
 })
 
 export { prisma as default }
