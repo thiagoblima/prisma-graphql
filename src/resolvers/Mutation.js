03347e50f1ab7d1b1a6f589889a33e223707b40f
Thiago Lima <thitheguy@gmail.com>
2020-05-29 01:55:56 -0300

    Adding login JWT ath token

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -21,6 +21,28 @@ const Mutation = {
             token: jwt.sign({ userId: user.id }, '61133F2EABE79CA4475F9CAB14124')
         }
     },
+    async login(parent, args, { prisma }, info) {
+        const user = await prisma.query.user({
+            where: {
+                email: args.data.email
+            }
+        })
+        
+        if (!user) {
+             throw new Error('Unable to login')
+        }
+
+        const isMatch = await bcrypt.compare(args.data.password, user.password)
+        
+        if (!isMatch) {
+            throw new Error('Unable to login')
+        }
+
+        return {
+            user,
+            token: jwt.sign({ userId: user.id }, '61133F2EABE79CA4475F9CAB14124')
+        }
+    },
     async deleteUser(parent, args, { prisma }, info) {
       const userExists = await prisma.exists.User({ id: args.id })
 
