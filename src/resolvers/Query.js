4c913b1a808b9021bf9663055e71e5caffe334c7
Thiago Lima <thitheguy@gmail.com>
2020-06-03 19:49:45 -0300

    Adding query for myPosts

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -18,6 +18,28 @@ const Query = {
         return prisma.query.users(opArgs, info)
      
     },
+    myPosts(parent, args, { prisma, request }, info) {
+        const userId = getUserId(request)
+        const opArgs = {
+            where: {
+                author: {
+                    id:userId
+                }
+            }
+        }
+        
+
+        if (args.query) {
+            opArgs.where.OR = [{
+                title_contains: args.query
+            }, {
+                body_contains: args.query
+            }]
+        }
+
+        return prisma.query.posts(opArgs, info)
+
+    },
     posts(parent, args, { prisma }, info) {
         const opArgs = {
             where: {
