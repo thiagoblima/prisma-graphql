0071ffecbd7cfddbac2b755f1ac0e5762ea4be26
Thiago Lima <thitheguy@gmail.com>
2020-06-03 22:05:56 -0300

    Adding fragment replacements

--- src/index.js
+++ src/index.js
@@ -3,26 +3,14 @@ import {
     PubSub
 } from 'graphql-yoga'
 import db from './db';
-import Query from './resolvers/Query'
-import Mutation from './resolvers/Mutation'
-import Subscription from './resolvers/Subscription'
-import User from './resolvers/User'
-import Post from './resolvers/Post'
-import Comment from './resolvers/Comment'
+import { resolvers, fragmentReplacements } from './resolvers/index'
 import prisma from './prisma'
 
 const pubsub = new PubSub()
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
-    resolvers: {
-        Query,
-        Mutation,
-        Subscription,
-        User,
-        Post,
-        Comment
-    },
+    resolvers,
     context(request) {
         return {
             db,
@@ -30,8 +18,8 @@ const server = new GraphQLServer({
             prisma,
             request
         }
-
-    }
+    },
+    fragmentReplacements
 })
 
 server.start((server) => {
--- src/prisma.js
+++ src/prisma.js
@@ -1,11 +1,13 @@
 import {
     Prisma
 } from 'prisma-binding'
+import { fragmentReplacements } from './resolvers/index'
 
 const prisma = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
     endpoint: 'http://localhost:4466',
-    secret: '61133F2EABE79CA4475F9CAB14124'
+    secret: '61133F2EABE79CA4475F9CAB14124',
+    fragmentReplacements
 })
 
 export { prisma as default }
