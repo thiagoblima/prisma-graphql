18fe27b7ddc00738e12bcd25e96d7bf1aee20f43
Thiago Lima <thitheguy@gmail.com>
2020-06-07 00:42:58 -0300

    Expiration token

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,15 +1,11 @@
 import bcrypt from 'bcryptjs'
 import getUserId from '../utils/getUserId'
 import generateToken from '../utils/generateToken'
+import hasPassword from '../utils/hashPassword'
 
 const Mutation = {
   async createUser(parent, args, { prisma }, info) {
-        if(args.data.password.length < 8) {
-            throw new Error('Password must be 8 characters or longer.')
-        }
-
-        const password = await bcrypt.hash(args.data.password, 10)
-
+        const password = await hasPassword(args.data.password) 
         const user = await prisma.mutation.createUser({ 
             data: {
               ...args.data,
@@ -63,6 +59,11 @@ const Mutation = {
     async updateUser(parent, args, { prisma, request }, info) {
         const userExists = await prisma.exists.User({ id: args.id })
         const userId = getUserId(request)
+
+
+        if (typeof args.data.password === 'string' ) {
+            args.data.password = await hasPassword(args.data.password)
+        }
 
         if (!userExists) {
             throw new Error('User not found')
