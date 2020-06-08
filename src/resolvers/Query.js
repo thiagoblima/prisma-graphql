53f3e2bc431b7f597834847408bf523f97924525
Thiago Lima <thitheguy@gmail.com>
2020-06-08 16:50:01 -0300

    Adding sorting orderBy

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -53,6 +53,7 @@ const Query = {
             first: args.first,
             skip: args.skip,
             after: args.after,
+            orderBy: args.orderBy,
             where: {
                 published: true
             }
@@ -77,6 +78,7 @@ const Query = {
             first: args.first,
             skip: args.skip,
             after: args.after,
+            orderBy: args.orderBy
         }
 
         return prisma.query.comments(opArgs, info)
