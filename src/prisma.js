bcdb80cb835c5f2608527cf51ec0070dfa23c951
Thiago Lima <thitheguy@gmail.com>
2020-06-24 19:30:29 -0300

    Adding a great package for secret API key as well as SECRET env settings

--- src/prisma.js
+++ src/prisma.js
@@ -4,7 +4,7 @@ import { fragmentReplacements } from './resolvers/index'
 const prisma = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
     endpoint: process.env.PRISMA_ENDPOINT,
-    secret: '61133F2EABE79CA4475F9CAB14124',
+    secret: process.env.PRISMA_SECRET,
     fragmentReplacements
 })
 
