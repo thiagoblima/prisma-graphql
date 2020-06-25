63a2d640f196505d5beb8c5e76106e43ecc4d7dc
Thiago Lima <thitheguy@gmail.com>
2020-06-25 18:31:29 -0300

    Adding JEST Assertions

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,7 +1,13 @@
 import { getFirstName } from '../src/utils/user'
 
-test('Testing users first name', () => {
+test('Should return only the individuals first name', () => {
       const firstName = getFirstName('Thiago Lima')
       
       expect(firstName).toBe('Thiago')
+})
+
+test('Should return first name when given first name', () => {
+    const firstName = getFirstName('Jen')
+
+    expect(firstName).toBe('Jen')
 })
