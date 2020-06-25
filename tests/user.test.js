09c5f9f3e428d242c9d710fc8f47ea9665d4eb83
Thiago Lima <thitheguy@gmail.com>
2020-06-25 18:27:23 -0300

    Adding JEST Assertions

--- tests/user.test.js
+++ tests/user.test.js
@@ -2,8 +2,6 @@ import { getFirstName } from '../src/utils/user'
 
 test('Testing users first name', () => {
       const firstName = getFirstName('Thiago Lima')
-
-      if (firstName !== 'Thiago') {
-        throw new Error('This should trigger a failure')
-      }
+      
+      expect(firstName).toBe('Thiago')
 })
