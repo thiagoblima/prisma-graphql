77af13a3ce201f160db6575a15a30402a7327c5e
Thiago Lima <thitheguy@gmail.com>
2020-06-25 23:12:31 -0300

    Splitting the server into a separate file

--- src/index.js
+++ src/index.js
@@ -1,24 +1,5 @@
-import "@babel/polyfill"
-import { GraphQLServer, PubSub } from 'graphql-yoga'
-import db from './db';
-import { resolvers, fragmentReplacements } from './resolvers/index'
-import prisma from './prisma'
-
-const pubsub = new PubSub()
-
-const server = new GraphQLServer({
-    typeDefs: './src/schema.graphql',
-    resolvers,
-    context(request) {
-        return {
-            db,
-            pubsub,
-            prisma,
-            request
-        }
-    },
-    fragmentReplacements
-})
+import "@babel/polyfill/noConflict"
+import server from './server'
 
 server.start({ port: process.env.PORT || 4000}, (server) => {
     console.log('The server is up!', server)
