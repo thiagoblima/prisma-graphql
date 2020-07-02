b769cbee86cd24436a557e8da2d1a157730c670e
Thiago Lima <thitheguy@gmail.com>
2020-07-02 15:46:29 -0300

    Adding expect for a specific users posts

--- tests/posts.test.js
+++ tests/posts.test.js
@@ -1,6 +1,6 @@
 import 'cross-fetch/polyfill'
 import {gql} from 'apollo-boost'
-import seedDatabase from './utils/seedDatabase'
+import seedDatabase, { userOne } from './utils/seedDatabase'
 import getClient from './utils/getClient'
 
 const client = getClient()
@@ -26,4 +26,22 @@ test('Should expose public posts', async () => {
 
     expect(response.data.posts.length).toBe(1)
     expect(response.data.posts[0].published).toBe(true)
+})
+
+test('Should fetch user posts', async () => {
+     const client = getClient(userOne.jwt)
+     const myPosts = gql`
+         query {
+             myPosts {
+                 id
+                 title
+                 body
+                 published
+             }
+         }
+     `
+
+     const { data } = await client.query({ query: myPosts })
+
+     expect(data.myPosts.length).toBe(2)
 })
